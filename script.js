3751c672a1fc47f3450143e205aba123974b6298
yeskapa <47764534+yeskapa@users.noreply.github.com>
2024-03-16 15:06:02 +1030

    added placing of new lines with mb2

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ var can
 var ctx
 
 const pathLength = 20
-const amountOfParticles = 100
+const amountOfParticles = 10
 const gravity = 0.1
 const friction = 1
-const collisionFriction = 0.9
+const collisionFriction = 0.8
 
 var lines = [
     {p1:{x:0, y:0}, p2:{x:innerWidth, y:0}},
@@ -13,6 +13,11 @@ var lines = [
     {p1:{x:innerWidth, y:innerHeight}, p2:{x:0, y:innerHeight}},
     {p1:{x:0, y:innerHeight}, p2:{x:0, y:0}},
 ]
+
+var newLine = {
+    p1:{x:0, y:0},
+    p2:{x:0, y:0}
+}
 
 var particles = []
 initParticles(amountOfParticles, pathLength)
@@ -39,12 +44,27 @@ addEventListener("mousemove", function(e) {
 
 addEventListener("mousedown", function(e) {
     if (e.button == 0) mouse.mouse1Down = true
-    else if (e.button == 2) mouse.mouse2Down = true
+    else if (e.button == 2) {
+        newLine.p1.x = e.clientX
+        newLine.p1.y = e.clientY
+        mouse.mouse2Down = true
+    }
 })
 
 addEventListener("mouseup", function(e) {
     if (e.button == 0) mouse.mouse1Down = false
-    else if (e.button == 2) mouse.mouse2Down = false
+    else if (e.button == 2) {
+        newLine.p2.x = e.clientX
+        newLine.p2.y = e.clientY
+
+        lines.push({p1:{x:newLine.p1.x, y:newLine.p1.y}, p2:{x:newLine.p2.x, y:newLine.p2.y}})
+        newLine = {
+            p1:{x:0, y:0},
+            p2:{x:0, y:0}
+        }
+
+        mouse.mouse2Down = false
+    }
 })
 
 function drawLines(color, width) {
@@ -54,6 +74,17 @@ function drawLines(color, width) {
         ctx.beginPath()
         ctx.moveTo(lines[i].p1.x, lines[i].p1.y)
         ctx.lineTo(lines[i].p2.x, lines[i].p2.y)
+        ctx.stroke()
+        ctx.closePath()
+    }
+}
+function drawNewLine() {
+    if (mouse.mouse2Down) {
+        ctx.strokeStyle = "#2a2a2a"
+        ctx.lineWidth = 2
+        ctx.beginPath()
+        ctx.moveTo(newLine.p1.x, newLine.p1.y)
+        ctx.lineTo(mouse.position.x, mouse.position.y)
         ctx.stroke()
         ctx.closePath()
     }
@@ -73,6 +104,7 @@ function update() {
         particles[i].draw("#fff", 2, 2, "#ffffff33")
     }
 
+    drawNewLine()
     drawLines("#fff", 2)
 
     window.requestAnimationFrame(update)
